67734f3be5b20f0f5a9906bf3bc8275f2b0ed583
Martin <loscarepapa@gmail.com>
2021-11-24 12:43:42 -0600

    git init

---
+++ lib/taxir.ex
@@ -0,0 +1,18 @@
+defmodule Taxir do
+  @moduledoc """
+  Documentation for `Taxir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Taxir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/taxir_test.exs
@@ -0,0 +1,8 @@
+defmodule TaxirTest do
+  use ExUnit.Case
+  doctest Taxir
+
+  test "greets the world" do
+    assert Taxir.hello() == :world
+  end
+end
